dbd849213d343ac81d6888f5895407bfb75d544a
thackerviraj <141397096+thackerviraj@users.noreply.github.com>
2023-09-04 12:38:51 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,18 @@
+(function () {
+    const typed = $('.typed');
+    if (typed) {
+            let typed_strings = typed.attr('data-typed-items');
+            typed_strings = typed_strings.split(',');
+            new Typed('.typed', {
+                    strings: typed_strings,
+                    loop: true,
+                    typeSpeed: 100,
+                    backSpeed: 50,
+                    backDelay: 2000
+            });
+    }
+})();
+
+$(
+    AOS.init()
+);
